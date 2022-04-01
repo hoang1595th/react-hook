70ee43caa339c094648dc049d06677d4f0941222
hoang1595th <hoang1595th@gmail.com>
2022-04-01 21:43:24 +0700

    test

--- src/components/useEffect/Child.js
+++ src/components/useEffect/Child.js
@@ -1,44 +1,44 @@
 import React, { useState, useEffect } from 'react';
 
 function SubChild() {
-    const [text, setText] = useState('I am sub-child component');
+	const [text, setText] = useState('I am sub-child component');
 
-    useEffect(() => {
-        const showWelcome = setTimeout(
-            () => setText('Welcome to sub-child')
-            , 5000
-        )
+	useEffect(() => {
+		const showWelcome = setTimeout(
+			() => setText('Welcome to sub-child')
+			, 5000
+		)
 
-        return () => {
-            clearTimeout(showWelcome)
-        }
-    }, []);
+		return () => {
+			clearTimeout(showWelcome)
+		}
+	}, []);
 
-    return (
-        <div >
-            {text}
-        </div>
-    );
+	return (
+		<div >
+			{text}
+		</div>
+	);
 }
 
 function Child({ index }) {
-    const [isDisplaySubChild, setIsDisplaySubChild] = useState(false);
+	const [isDisplaySubChild, setIsDisplaySubChild] = useState(false);
 
-    // update stateString when index change value
-    useEffect(() => {
-        if (index % 2 === 0) {
-            setIsDisplaySubChild(true)
-        } else {
-            setIsDisplaySubChild(false)
-        }
-    }, [index]);
+	// update stateString when index change value
+	useEffect(() => {
+		if (index % 2 === 0) {
+			setIsDisplaySubChild(true)
+		} else {
+			setIsDisplaySubChild(false)
+		}
+	}, [index]);
 
-    return (
-        <div>
-            <div>child component:</div>
-            {isDisplaySubChild && <SubChild />}
-        </div>
-    );
+	return (
+		<div>
+			<div>child component:</div>
+			{isDisplaySubChild && <SubChild />}
+		</div>
+	);
 }
 
 export default Child;
